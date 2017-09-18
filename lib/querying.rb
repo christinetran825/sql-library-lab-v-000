8f0444303ea982cfb2ee8af9f0796d5b3d391a6c
Christine Tran <christinetran825@gmail.com>
2017-09-18 06:50:00 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters ON series.title = characters.series_id
   WHERE characters.species = 'human'
-  GROUP BY ;"
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
