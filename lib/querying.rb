ebe317e9cc923154615a914e44d4e3c705dae3b2
Christine Tran <christinetran825@gmail.com>
2017-09-18 06:47:07 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,17 +1,25 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year FROM books WHERE series_id = 1;"
+  "SELECT title, year FROM books 
+  WHERE series_id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters WHERE LENGTH(motto) = (SELECT MAX(LENGTH(motto)) FROM characters);"
+  "SELECT name, motto FROM characters 
+  WHERE LENGTH(motto) = (SELECT MAX(LENGTH(motto)) FROM characters);"
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1;"
+  "SELECT species, COUNT(species)
+  FROM characters
+  GROUP BY species
+  ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors JOIN subgenres;"
+  "SELECT authors.name, subgenres.name
+  FROM series
+  JOIN authors ON series.author_id = authors_id
+  JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
