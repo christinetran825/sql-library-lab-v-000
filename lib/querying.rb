60efa2c108cdcf80fed75d4651c3d40d38c47070
Christine Tran <christinetran825@gmail.com>
2017-09-18 06:27:05 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT titles, years FROM books WHERE series_id = 1"
+  "SELECT title, years FROM books WHERE series_id = 1"
 end
 
 it 'selects all of the books titles and years in the first series and orders them chronologically' do
