feeffa1e8c23302d2b7102b2e53ad8edc119098c
Christine Tran <christinetran825@gmail.com>
2017-09-18 06:24:02 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -8,35 +8,35 @@ CREATE TABLE series (
 
 CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
-    name TEXT
+  name TEXT
 );
 
 CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
-    name TEXT
+  name TEXT
 );
 
 --has many characters
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
-    title TEXT,
-    year INTEGER,
-    series_id INTEGER
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
 );
 
 -- are in many books in a series
 CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
-    name TEXT,
-    motto TEXT,
-    species TEXT,
-    author_id INTEGER,
-    series_id INTEGER
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
 );
 
 --characters & books
 CREATE TABLE character_books (
   id INTEGER PRIMARY KEY,
-    character_id INTEGER,
-    book_id TEXT
+  character_id INTEGER,
+  book_id TEXT
 );
