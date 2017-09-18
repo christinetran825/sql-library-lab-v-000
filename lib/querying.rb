550bf5a9aae88d8e35e806a5eb988cf1c8152e0a
Christine Tran <christinetran825@gmail.com>
2017-09-18 06:27:41 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,7 +2,6 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT title, years FROM books WHERE series_id = 1"
 end
 
-
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT"
 end
